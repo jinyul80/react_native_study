bca89892ea36799b994ea4ee90ee57f499efe565
Jinyeol Kim <jinyul80@gmail.com>
2026-01-14 23:30:21 +0900

    숫자 입력 스크린 추가

---
+++ rn_game1/components/PrimaryButton.js
@@ -0,0 +1,39 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={({ pressed }) =>
+                    pressed
+                        ? [styles.buttonInnerContainer, styles.pressed]
+                        : styles.buttonInnerContainer
+                }
+                onPress={() => console.log("Pressed...")}
+                android_ripple={{ color: "#b80860" }}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden",
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#72063c",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 4,
+    },
+    buttonText: { color: "white", textAlign: "center" },
+    pressed: {
+        opacity: 0.75,
+    },
+});
---
+++ rn_game1/screens/StartGameScreen.js
@@ -0,0 +1,52 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+            />
+            <View style={styles.buttonContainer}>
+                <View style={{ flex: 1 }}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={{ flex: 1 }}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: "#4e0329",
+        borderRadius: 8,
+        elevation: 4, // Android 그림자 설정
+    },
+    numberInput: {
+        height: 60,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        flex: 1,
+    },
+});
