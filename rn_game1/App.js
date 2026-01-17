dc844f8696e2d4f5e1b3a89f75623d84a5411edd
Jinyeol Kim <jinyul80@gmail.com>
2026-01-17 23:10:47 +0900

    숫자 추측하는 로직 추가 및 크다, 작다 선택 버튼 추가

--- rn_game1/App.js
+++ rn_game1/App.js
@@ -29,7 +29,7 @@ export default function App() {
                 >
                     <SafeAreaView style={styles.rootScreen}>
                         {userNumber ? (
-                            <GameScreen />
+                            <GameScreen userNumber={userNumber} />
                         ) : (
                             <StartGameScreen
                                 onPickNumberFn={pickedNumberHandler}
--- rn_game1/screens/GameScreen.js
+++ rn_game1/screens/GameScreen.js
@@ -1,13 +1,74 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import Title from "../components/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/PrimaryButton";
 
-const GameScreen = () => {
+const generateRandomBetween = (min, max, exclude) => {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+};
+
+let minBoundary = 1;
+let maxBoundray = 100;
+
+const GameScreen = ({ userNumber }) => {
+    const initialGuess = generateRandomBetween(
+        minBoundary,
+        maxBoundray,
+        userNumber
+    );
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    const nextGuessHandler = (direction) => {
+        if (
+            (direction == "lower" && currentGuess < userNumber) ||
+            (direction == "greater" && currentGuess > userNumber)
+        ) {
+            Alert.alert("Don't lie!!!", "You know that this is wrong...", [
+                { text: "Sorry!", style: "cancel" },
+            ]);
+            return;
+        }
+        if (direction == "lower") {
+            maxBoundray = currentGuess;
+        } else {
+            minBoundary = currentGuess + 1;
+        }
+
+        const newRndNumber = generateRandomBetween(
+            minBoundary,
+            maxBoundray,
+            currentGuess
+        );
+
+        setCurrentGuess(newRndNumber);
+    };
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
-            {/* GUESS */}
+            <NumberContainer>{currentGuess}</NumberContainer>
             <View>
                 <Text>Higher or lower?</Text>
+                <View>
+                    <PrimaryButton
+                        onPressFn={nextGuessHandler.bind(this, "lower")}
+                    >
+                        -
+                    </PrimaryButton>
+                    <PrimaryButton
+                        onPressFn={nextGuessHandler.bind(this, "greater")}
+                    >
+                        +
+                    </PrimaryButton>
+                </View>
+
                 {/* +- */}
             </View>
             <View>{/* LOG Rounded.. */}</View>
