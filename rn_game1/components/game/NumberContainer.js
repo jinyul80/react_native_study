2d362356a712dbe176395e092b9d410484eeb62c
Jinyeol Kim <jinyul80@gmail.com>
2026-01-26 23:02:28 +0900

    기기의 크기를 가져와서 동적으로 크기 적용하기
    - Dimensions API 적용

--- rn_game1/components/game/NumberContainer.js
+++ rn_game1/components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 const NumberContainer = ({ children }) => {
@@ -11,12 +11,15 @@ const NumberContainer = ({ children }) => {
 
 export default NumberContainer;
 
+// 기기의 앱 영역 width
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
     container: {
         borderWidth: 4,
         borderColor: Colors.accent500,
-        padding: 24,
-        margin: 24,
+        padding: deviceWidth < 380 ? 12 : 24,
+        margin: deviceWidth < 380 ? 12 : 24,
         borderRadius: 8,
         alignItems: "center",
         justifyContent: "center",
@@ -24,6 +27,6 @@ const styles = StyleSheet.create({
     numberText: {
         color: Colors.accent500,
         fontFamily: "OpenSans-Bold",
-        fontSize: 36,
+        fontSize: deviceWidth < 380 ? 28 : 36,
     },
 });
