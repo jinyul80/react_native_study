c73048eaf9f4120a44d9baa371fea6fc90aa0a6c
Jinyeol Kim <jinyul80@gmail.com>
2026-02-02 22:47:04 +0900

    Navigation 패키지 설치 및 화면 이동 기능 추가

--- rn_food/App.js
+++ rn_food/App.js
@@ -1,18 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-      <SafeAreaProvider>
-          <StatusBar style="light" />
-          <SafeAreaView style={styles.rootScreen}>
-              <CategoriesScreen />
-          </SafeAreaView>
-      </SafeAreaProvider>
-  );
+    return (
+        <>
+            <StatusBar style="dark" />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="MealsCategories"
+                        component={CategoriesScreen}
+                    />
+                    <Stack.Screen
+                        name="MealsOverview"
+                        component={MealsOverviewScreen}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
--- rn_food/screens/CategoriesScreen.js
+++ rn_food/screens/CategoriesScreen.js
@@ -3,16 +3,23 @@ import { FlatList } from "react-native";
 
 import CategoryGridTile from "../components/CategoryGridTile";
 
-const renderCategoryItem = (itemData) => {
-    return (
-        <CategoryGridTile
-            title={itemData.item.title}
-            color={itemData.item.color}
-        />
-    );
-};
 
-const CategoriesScreen = () => {
+
+const CategoriesScreen = ({ navigation }) => {
+    const renderCategoryItem = (itemData) => {
+        const pressHandler = () => {
+            navigation.navigate("MealsOverview");
+        };
+
+        return (
+            <CategoryGridTile
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onPress={pressHandler}
+            />
+        );
+    };
+
     return (
         <FlatList
             data={CATEGORIES}
